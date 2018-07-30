4290e2b43bf48c1d65025403856eabc263ac8cb0
Samuel Billaud <samuel.billaud1990@gmail.com>
2018-07-31 01:24:24 +0200

    Add InputRadio component

---
+++ client/src/components/InputRadio.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="input-radio">
+    <input
+      type="radio"
+      :name="name"
+      :id="id"
+      :value="value"
+      @change="updateValue($event.target.value)">
+      <label :for="id">
+        {{ id }}
+      </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputRadio',
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+    id: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    updateValue (value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
