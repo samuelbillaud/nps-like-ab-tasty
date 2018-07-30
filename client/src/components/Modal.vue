229e6a206f68643000154378f0aae2952d7903cf
Samuel Billaud <samuel.billaud1990@gmail.com>
2018-07-31 01:42:30 +0200

    Reindent

--- client/src/components/Modal.vue
+++ client/src/components/Modal.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="modal-overlay" v-show="isActive">
-        <div class="modal-content">
-            <span class="close-button" @click="closeModal">&times;</span>
-            <h1>{{ question }}</h1>
-            <form @submit.prevent="onSubmit">
-                <ul>
-                    <li v-for="(item, index) in notes" :key="index">
-                      <input-radio
-                        name="note"
-                        :id="item"
-                        :value="item"
-                        :valueRadio="item"
-                        @input="chooseNote"
-                      ></input-radio>
-                    </li>
-                </ul>
+    <div class="modal-content">
+      <span class="close-button" @click="closeModal">&times;</span>
+      <h1>{{ question }}</h1>
+      <form @submit.prevent="onSubmit">
+          <ul>
+              <li v-for="(item, index) in notes" :key="index">
+                <input-radio
+                  name="note"
+                  :id="item"
+                  :value="item"
+                  :valueRadio="item"
+                  @input="chooseNote"
+                ></input-radio>
+              </li>
+          </ul>
 
-                <input-text
-                  name="comment"
-                  placeholder="Votre message ici"
-                  @input="writeComment"
-                ></input-text>
+          <input-text
+            name="comment"
+            placeholder="Votre message ici"
+            @input="writeComment"
+          ></input-text>
 
-                <button :disabled="!userNote || !comment">{{ valid }}</button>
-            </form>
-        </div>
+          <button :disabled="!userNote || !comment">{{ valid }}</button>
+      </form>
+    </div>
   </div>
 </template>
 
